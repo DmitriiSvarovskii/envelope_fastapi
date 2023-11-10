7895a30a4b2777e2e78fb2252ed158bc74af1d56
DmitriiSvarovskii <swarovskidima@gmail.com>
2023-11-11 00:20:54 +0530

    fix

--- src/api_admin/customer/schemas.py
+++ src/api_admin/customer/schemas.py
@@ -5,23 +5,23 @@ class CustomerBase(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     id: int
     tg_user_id: int
-    first_name: str
-    last_name: str
-    username: str
-    is_premium: bool
-    query_id: str
-    hash: str
+    # first_name: str
+    # last_name: str
+    # username: str
+    # is_premium: bool
+    # query_id: str
+    # hash: str
 
 
 class CustomerCreate(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     tg_user_id: int
-    first_name: str
-    last_name: str
-    username: str
-    is_premium: bool
-    query_id: str
-    hash: str
+    # first_name: str
+    # last_name: str
+    # username: str
+    # is_premium: bool
+    # query_id: str
+    # hash: str
 
 
 class CustomerUpdate(CustomerCreate):
