0c3a54f604dddebf418e0290feb49b1c88dcf2ba
DmitriiSvarovskii <swarovskidima@gmail.com>
2023-11-23 00:34:49 +0530

    fix

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.api_admin.routers import routers
-import aiohttp
 
 
 app = FastAPI(
@@ -12,27 +11,6 @@ app = FastAPI(
     redoc_url=None,
 )
 
-
-async def send_webhook(url):
-    async with aiohttp.ClientSession() as session:
-        payload = {"url": url}
-        headers = {"Content-Type": "application/json"}
-
-        async with session.post('https://api.telegram.org/bot6616951235:AAEcMXkGdXNTibeVr1j2zMu3CHLF1A8l_2A/setWebhook',
-                                json=payload, headers=headers) as response:
-            return await response.json()
-
-
-@app.post("/")
-async def test():
-    ngrok_url = "https://envelope-app.ru"
-    response = await send_webhook(ngrok_url)
-    print(response)
-    return {"ok": True}
-
-# @app.post("/")
-# async def test():
-#     pass
 
 ORIGINS = [
     "http://localhost",
