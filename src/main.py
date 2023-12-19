35c9e9d4c9bb6de61af20795f35a02123f3f10b5
DmitriiSvarovskii <swarovskidima@gmail.com>
2023-12-19 18:10:08 +0530

    fix

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from aiogram.types import Update
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from src.api_admin.routers import routers
@@ -17,6 +18,38 @@ app = FastAPI(
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+WEBHOOK_HOST = 'https://envelope-app.ru'
+# WEBHOOK_HOST = 'https://fd9f-103-157-162-242.ngrok.io'
+WEBHOOK_PATH = '/api/v1/webhook'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+
+@app.on_event("startup")
+async def on_startup():
+    webhook_info = await bot.get_webhook_info()
+    if webhook_info.url != WEBHOOK_URL:
+        await bot.set_webhook(
+            url=WEBHOOK_URL
+        )
+
+
+@app.post(WEBHOOK_PATH)
+async def bot_webhook(request: Request):
+    update_data = await request.json()
+    print("Полученные данные обновления:", update_data)
+    update = Update(**update_data)
+    await dp.feed_update(bot, update)
+
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    await bot.session.close()
+
+
+@dp.message(Command(commands=['start']))
+async def start(message: types.Message):
+    await bot.send_message(chat_id=message.chat.id, text="Привет! Я ваш бот.")
 
 
 ORIGINS = [
@@ -41,17 +74,3 @@ app.add_middleware(
 
 for router in routers:
     app.include_router(router)
-
-
-@app.post('/api/v1/webhook')
-async def receive_update(request: Request):
-    # Десериализация обновления Telegram
-    update = await request.json()
-    # Обновление диспетчера
-    await dp.process_update(types.Update.to_object(update))
-    return {'ok': True}
-
-
-@dp.message(Command(commands=['/start']))
-async def start(message: types.Message):
-    await bot.send_message(message.chat.id, "Привет! Я ваш бот.")
