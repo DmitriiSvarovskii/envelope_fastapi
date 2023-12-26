2537d285b428f65dea722bb6ef91c072200f5ca4
DmitriiSvarovskii <swarovskidima@gmail.com>
2023-12-26 17:05:56 +0530

    log

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,3 +1,4 @@
+import logging
 from fastapi import HTTPException, Request, APIRouter
 from aiogram import Bot, Dispatcher
 from aiogram.types import Update
@@ -14,32 +15,57 @@ router = APIRouter(
     prefix=f"{WEBHOOK_PATH}",
     tags=["Webhook (telegram_bot)"])
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 @router.post("/{token}")
 async def bot_webhook(request: Request, token: str):
+    # Логируем получение обновления
     update_data = await request.json()
+    logger.info(f"Received update for token {token}: {update_data}")
+
+    # Создание объекта Update
     update = Update(**update_data)
+
+    # Построение URL вебхука и поиск пары bot, dp
     webhook_url = f"{WEBHOOK_HOST}{WEBHOOK_PATH}/{token}"
     pair = dispatchers_by_webhook_url.get(webhook_url)
+
+    # Логирование в случае, если бот не найден
     if not pair:
+        logger.error(f"Bot not found for webhook URL: {webhook_url}")
         raise HTTPException(status_code=404, detail="Bot not found")
+
+    # Получение бота и диспетчера, отправка обновления диспетчеру
     bot, dp = pair
     await dp.feed_update(bot, update)
+    logger.info(f"Update for token {token} fed to dispatcher")
 
 
 @router.on_event("startup")
 async def on_startup_bot():
+    logger.info("Starting up bots")
     global bots
     async for session in get_async_session():
-        store = await get_info_store_token_all(session)
-        tokens = [{"token_bot": bot.token_bot} for bot in store]
+        try:
+            store = await get_info_store_token_all(session)
+            tokens = [{"token_bot": bot.token_bot} for bot in store]
+            logger.info(f"Found {len(tokens)} bots in the database")
+        except Exception as e:
+            logger.error(f"Error in getting tokens from database: {e}")
+            continue
     for token_info in tokens:
         token = token_info["token_bot"]
-        bot, dp = await create_bot(token)
-        bots.append(bot)
-        dispatchers_by_webhook_url[f"{WEBHOOK_HOST}{WEBHOOK_PATH}/{token}"] = (
-            bot, dp)
-        await setup_webhook_for_bot(bot, f"{WEBHOOK_HOST}{WEBHOOK_PATH}/{token}")
+        try:
+            bot, dp = await create_bot(token)
+            bots.append(bot)
+            webhook_url = f"{WEBHOOK_HOST}{WEBHOOK_PATH}/{token}"
+            dispatchers_by_webhook_url[webhook_url] = (bot, dp)
+            await setup_webhook_for_bot(bot, webhook_url)
+            logger.info(f"Bot with token {token} started and webhook set")
+        except Exception as e:
+            logger.error(f"Error in creating bot with token {token}: {e}")
 
 
 @router.on_event("shutdown")
